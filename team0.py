11e42387c384f16d058ebe86269eb8ef204e2120
paigelabarr <paige_labarr@pittsford.monroe.edu>
2016-07-28 08:08:43 -0400

    updated team0 again

--- team0.py
+++ team0.py
@@ -7,7 +7,7 @@
 ####
 
 team_name = 'Paige_wins' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
+strategy_name = 'The Pikachu of all strategies'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
