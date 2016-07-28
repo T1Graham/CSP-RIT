78f4f86f03264914543b8d3af7aad0a2cd81cdb9
afshsully <tsullivan@wcpss.net>
2016-07-28 08:23:19 -0400

    Holds a grudge
    Sully team5 changes

--- team5.py
+++ team5.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'T5' # Only 10 chars displayed.
+strategy_name = 'Grudge'
+strategy_description = 'Holds a grudge'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -26,6 +26,12 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
+    if len(my_history)<2: # It's the first two rounds; be nice.
+        return 'c'
+    if 'b' in their_history[-5:]: # If the other player has betrayed within last 5 rounds, 
+        return 'b'                # Betray.   
+        
+    # Collude if not first couple rounds and haven't been betrayed stay nice
     return 'c'
 
     
