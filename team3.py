67469dc51eb1507a103b5bd637291c771eb3fb1d
ReidPalMac <rachelle.reid@palmaccsd.org>
2016-07-27 21:11:03 -0400

    Team 3 changed

--- team3.py
+++ team3.py
@@ -6,10 +6,10 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
-    
+team_name = 'Team 3' 
+strategy_name = "Colluding FTW"
+strategy_description = 'If opponent colluded last turn, I collude. Every 3 turns I betray, or when my score is less than theirs I betray'
+
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
@@ -26,7 +26,18 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    roundNumber = len(my_history)
+       
+    if roundNumber > 1 and their_history[-1] == 'c':
+        return 'c'
+    
+          
+    if roundNumber % 3 == 0:
+        return 'b'
+        
+        
+    if my_score < their_score: 
+        return 'b' 
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
