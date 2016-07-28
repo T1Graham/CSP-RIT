204a340e43dc88027518024e135c9f86d12e4315
K1NGK0 <NKohlbatz@GMail.com>
2016-07-28 08:53:47 -0400

    Strategy to Kill the competition...

--- team12.py
+++ team12.py
@@ -18,17 +18,32 @@ def move(my_history, their_history, my_score, their_score):
     Returns 'c' or 'b'. 
     '''
 
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
+    if (len(my_history)) == 0:
+            return "c"
+            
+    elif (len(my_history))%2 == 0:
     
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
+        if their_history[-1] == "b":
+            return "b"
+        else:
+            return "c"
+
+    else:
+        if their_history[-1] == "b":
+            return "c"
+        else:
+            return "c"
     
-    return 'c'
+    
+   
+   
+   
+
 
     
+        
+            
+                    
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
