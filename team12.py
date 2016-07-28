537df87a04bb4fa80c5d920d0ac45f8e29171576
K1NGK0 <NKohlbatz@GMail.com>
2016-07-28 09:13:46 -0400

    Try this one...

--- team12.py
+++ team12.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'King Kong' # Only 10 chars displayed.
+strategy_name = 'Every other round the strategy switchs, KK colludes often'
+strategy_description = 'It uses a modulus to decide if its and even or odd round'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -16,7 +16,7 @@ def move(my_history, their_history, my_score, their_score):
     
     Make my move.
     Returns 'c' or 'b'. 
-    '''
+    '''    
 
     if (len(my_history)) == 0:
             return "c"
